5e469ac5d4fca83ab5108a622cc6087dcb796fc6
玉羽 <yiyouyuyu@outlook.com>
2018-12-28 18:50:31 +0800

    添加项目文件。

---
+++ MCode/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace MCode {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        /// <summary>
+        /// 当前打开的文件路径
+        /// </summary>
+        private string FilePath { get; set; }
+
+        /// <summary>
+        /// 文本编辑框体
+        /// </summary>
+        private TextBox MTextBox {
+            get => textBox;
+            set => textBox = value;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void Title_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            DragMove();
+        }
+
+        /// <summary>
+        /// 在状态栏跟踪光标位置
+        /// </summary>
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e) {
+            //光标位置
+            int index = MTextBox.SelectionStart;
+            int row = 1,
+                col = 1;
+            //从光标处往前遍历
+            int i;
+            for (i = index - 1; i >= 0; i -= 1) {
+                if (MTextBox.Text[i] == '\n') row += 1;
+                if (row == 1) col += 1;
+            }
+            textBoxInformation.Text = " 第 " + row + " 行；第 " + col + " 列";
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
---
+++ MCode/MuneBar/File.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MCode {
+    public partial class MainWindow : Window {
+
+        /// <summary>
+        /// File->Open，打开文件
+        /// </summary>
+        private void Open_Click(object sender, RoutedEventArgs e) {
+            //选择文件
+            var openFileDialog = new OpenFileDialog() {
+                Filter = "文本文件(*.txt)|*.txt|所有文件(*.*)|*.*"
+            };
+            //result是选择文件窗体的点击结果
+            var result = openFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK) {
+                //文件路径
+                FilePath = openFileDialog.FileName;
+                var fileStream = new FileStream(FilePath, FileMode.Open);
+                //每次读8192个字节
+                var buffer = new byte[8192];
+                //r是一次读取的量
+                var r = fileStream.Read(buffer, 0, buffer.Length);
+                //清空现有的textbox
+                if (MTextBox.Text.Length > 0) {
+                    MTextBox.Text = "";
+                }
+                while (r == 8192) {
+                    //读满表示没读完
+                    MTextBox.Text += Encoding.Default.GetString(buffer, 0, r);
+                    r = fileStream.Read(buffer, 0, buffer.Length);
+                }
+                MTextBox.Text += Encoding.Default.GetString(buffer, 0, r);
+                fileStream.Close();
+            } else {
+                //其他的情况，若要增加要使用switch
+                return;
+            }
+        }
+
+        /// <summary>
+        /// File->Save，保存文件
+        /// </summary>
+        private void Save_Click(object sender, RoutedEventArgs e) {
+            if (FilePath == null) {
+                //新创建的文件或者被SaveAs_Click调用
+                var saveFileDialog = new SaveFileDialog() {
+                    Filter = "文本文件(*.txt)|*.txt|所有文件(*.*)|*.*",
+                    FileName = "未命名"
+                };
+                //result是保存文件窗体的点击结果
+                var result = saveFileDialog.ShowDialog();
+                if(result== System.Windows.Forms.DialogResult.OK) {
+                    //文件路径
+                    FilePath = saveFileDialog.FileName;
+                } else {
+                    //其他的情况，若要增加要使用switch
+                    return;
+                }
+            }
+            var fileStream = new FileStream(FilePath, FileMode.Create);
+            var buffer = Encoding.Default.GetBytes(MTextBox.Text);
+            fileStream.Write(buffer, 0, buffer.Length);//！！！这里有个问题，一次的量可能会超过int，要改！！！
+            fileStream.Close();
+        }
+        
+        /// <summary>
+        /// 另存为
+        /// </summary>
+        private void SaveAs_Click(object sender, RoutedEventArgs e) {
+            //选择保存地址，暂时无法解决代码重用问题
+            var saveFileDialog = new SaveFileDialog() {
+                Filter = "文本文件(*.txt)|*.txt|所有文件(*.*)|*.*",
+                FileName = "未命名"
+            };
+            //result是保存文件窗体的点击结果
+            var result = saveFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK) {
+                //文件路径
+                FilePath = saveFileDialog.FileName;
+            } else {
+                //其他的情况，若要增加要使用switch
+                return;
+            }
+            Save_Click(sender,e);
+        }
+
+        
+    }
+}
